93fb601005b9abfcc9e88803a13f28f8e3a7b339
Minh Tri <epsminhtri@gmail.com>
2024-01-02 16:25:06 +0700

    uodate code

--- sources/app/routes/index.py
+++ sources/app/routes/index.py
@@ -5,3 +5,22 @@ index_bp = Blueprint('index', __name__)
 @index_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@index_bp.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+@index_bp.route('/help', methods=['GET'])
+def help():
+    return render_template('help.html')
+
+@index_bp.route('/tips', methods=['GET'])
+def tips():
+    return render_template('tips.html')
+
+
+@index_bp.route('/law', methods=['GET'])
+def law():
+    return render_template('law.html')
