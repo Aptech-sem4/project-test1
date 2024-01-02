1e4def231c5dc7670df553a048c5f56924c93a6e
Minh Tri <epsminhtri@gmail.com>
2024-01-02 11:43:29 +0700

    update

--- sources/app/__init__.py
+++ sources/app/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app
+import  os
 
 app = Flask(__name__)
 
 # Thiết lập cấu hình
 app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
+
+# Thực hiện các hoạt động yêu cầu ngữ cảnh ứng dụng ở đây
+root_path_folder = current_app.root_path
+# Lấy thư mục chứa file hiện tại
+current_directory = root_path_folder + '/models'
+MODEL_PATH = os.path.join(current_directory, 'model_EfficientnetB0.h5')
+
 
 # Import Blueprint và đăng ký Blueprint vào app
 from app.routes import upload, index
