92ee2c164d6e97cb8eb068922a1baa55d864cba2
Minh Tri <epsminhtri@gmail.com>
2024-01-03 08:22:06 +0700

    update model

--- sources/app/models/img_process.py
+++ sources/app/models/img_process.py
@@ -1,20 +1,11 @@
 from flask import Blueprint, render_template, jsonify
-import os
+import os, numpy as np
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from flask import current_app
 from app import MODEL_PATH
 
-# # from tensorflow.keras.applications import EfficientNetB0
-# import numpy as np
-# import pickle
-
-# predict_bp = Blueprint('predict', __name__)
-
 mymodel = load_model(MODEL_PATH)
-
-# # Load pre-trained model
-# model = EfficientNetB0(weights=MODEL_PATH)
     
 def process_image(filename):
     # Lấy đường dẫn tuyệt đối của file hiện tại
@@ -23,18 +14,20 @@ def process_image(filename):
     
     absolute_path = root_path + folder_name
     file_path = os.path.join(absolute_path, filename)
-     # Load and preprocess the image
-    print('111111111 vao img process')
-    print(file_path)
+
     img = image.load_img(file_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255
     print('22222 vao img process')
-    print(img)
+    print(img_array)
     # img_array = image.img_to_array(img)
     # img_array = np.expand_dims(img_array, axis=0)
     # processed_img = EfficientNetB0.preprocess_input(img_array)
 
     # Make prediction
-    predictions = mymodel.predict(img)
+    predictions = "test"
+    # predictions = mymodel.predict(img_array)
     # Decode prediction
     # decoded_predictions = EfficientNetB0.decode_predictions(predictions)
     
